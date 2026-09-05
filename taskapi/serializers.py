8d4df065824ae6b735cfa72e8e1387b718a63e8a
Rahul Verma <rrrahulj2006@gmail.com>
2026-09-05 11:25:58 +0530

    Final

--- taskapi/serializers.py
+++ taskapi/serializers.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 from .models import TaskManager
 
 class managingserializer(serializers.HyperlinkedModelSerializer):
+    id = serializers.ReadOnlyField()
+
     class Meta:
         model = TaskManager
         fields = "__all__"
